f1d9b2fca59a6a6ad334a6d33b5736f9f6d2ae6f
Darshaka Pathirana <dpat@syn-net.org>
2010-05-04 02:16:12 +0200

    Added more config files

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,8 +3,16 @@
 SCRIPT=$(readlink -f $0)
 CONFIGS=$(dirname ${SCRIPT})
 
+
+for config in ${CONFIGS}/.*; do
+	echo $config
+done
+
 ln -s ${CONFIGS}/.zshrc ${HOME}/.zshrc
+ln -s ${CONFIGS}/.zshrc.local ${HOME}/.zshrc.local
 ln -s ${CONFIGS}/.vimrc ${HOME}/.vimrc
 ln -s ${CONFIGS}/.screenrc ${HOME}/.screenrc
 ln -s ${CONFIGS}/.todo ${HOME}/.todo
+ln -s ${CONFIGS}/.gitconfig ${HOME}/.gitconfig
+ln -s ${CONFIGS}/.hgrc ${HOME}/.hgrc
 
