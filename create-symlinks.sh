561327d558dd7f02f83b1e66fa863ae1837953b1
Darshaka Pathirana <dpat@syn-net.org>
2010-05-12 09:52:23 +0200

    added colorscheme for gvim

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -13,6 +13,7 @@ ln -s ${CONFIGS}/.zshrc.local ${HOME}/.zshrc.local
 ln -s ${CONFIGS}/.vim ${HOME}
 ln -s ${CONFIGS}/.vimrc ${HOME}/.vimrc
 ln -s ${CONFIGS}/.vimrc.local ${HOME}/.vimrc.local
+ln -s ${CONFIGS}/.gvimrc ${HOME}/.gvimrc
 ln -s ${CONFIGS}/.screenrc ${HOME}/.screenrc
 ln -s ${CONFIGS}/.todo ${HOME}
 ln -s ${CONFIGS}/.gitconfig ${HOME}/.gitconfig
