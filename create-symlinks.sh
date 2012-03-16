85c54bd4741306ea3c529f13469892490c4700d8
Darshaka Pathirana <dpat@syn-net.org>
2012-03-17 00:56:00 +0100

    improved create-symlinks
    + ignore .git
    + better output

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -7,14 +7,23 @@ for config in ${CONFDIR}/.[^.]?*; do
 	BASE=$(basename ${config})
 	HOMEBASE=${HOME}/${BASE}
 	BACKUP="${HOMEBASE}.$(date --rfc-3339=seconds)"
+
+	if [ "${BASE}" == ".git" ]; then
+		continue
+	fi
+
 	if [ -e ${HOMEBASE} ] && [ ! -L ${HOMEBASE} ]; then
 		mv ${HOMEBASE} "${BACKUP}"
 		echo "Backup of ${HOMEBASE} created -> ${BACKUP}"
 	fi
 
-	[ -L ${HOMEBASE} ] && rm ${HOMEBASE}
-	ln -sf ${config} ${HOMEBASE}
-	echo Created: ${HOMEBASE}
+	if [ -L ${HOMEBASE} ]; then
+		rm ${HOMEBASE}
+		echo "Recreated: ${HOMEBASE}"
+	else
+		ln -sf ${config} ${HOMEBASE}
+		echo "Created (new): ${HOMEBASE}"
+	fi
 done
 
 exit 0
