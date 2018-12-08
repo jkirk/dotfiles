335423fabf3ff07577a3572cda7e6c0ec7125b9d
Darshaka Pathirana <dpat@syn-net.org>
2018-12-08 14:16:31 +0100

    Update to grml-config per 2018-12-08 and add .tmux.conf

--- grml-config.sh
+++ grml-config.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 wget -O .screenrc     http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic
-wget -O .vimrc        http://git.grml.org/f/grml-etc-core/etc/vim/vimrc 
+wget -O .vimrc        http://git.grml.org/f/grml-etc-core/etc/vim/vimrc
 wget -O .zshrc        http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+wget -O .tmux         http://git.grml.org/f/grml-etc-core/etc/tmux.conf
 # optional:
 # wget -O .zshrc.local        http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
 echo "Optionally you might want to run:"
