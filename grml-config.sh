7bfcae61f1068c8595b8b22aa7381a145d6a7c9e
Darshaka Pathirana <dpat@syn-net.org>
2018-12-29 22:51:42 +0100

    grml-config.sh: Fix .tmux.conf wget

--- grml-config.sh
+++ grml-config.sh
@@ -2,7 +2,7 @@
 wget -O .screenrc     http://git.grml.org/f/grml-etc-core/etc/grml/screenrc_generic
 wget -O .vimrc        http://git.grml.org/f/grml-etc-core/etc/vim/vimrc
 wget -O .zshrc        http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
-wget -O .tmux         http://git.grml.org/f/grml-etc-core/etc/tmux.conf
+wget -O .tmux.conf    http://git.grml.org/f/grml-etc-core/etc/tmux.conf
 # optional:
 # wget -O .zshrc.local        http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
 echo "Optionally you might want to run:"
