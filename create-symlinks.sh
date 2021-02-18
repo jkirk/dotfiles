6ef2bf2a195f620eed98e5ee7a3ef90d6da7969e
Darshaka Pathirana <dpat@syn-net.org>
2021-02-18 11:28:28 +0100

    create-symlinks.sh: Include config files in .config directory
    $HOME/.config holds a lot of config files. Moving everyting to
    $HOME/configs did not seem right.
    Therefor rewrote create-symlinks.sh to iterarte the existing config
    files in .config.
    
    Also fixed some shellcheck warnings and introduced a --force flag to
    force recreation of the symlinks. If it is not given, the existing
    symlinks are not touched.

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-SCRIPT=$(readlink -f $0)
-CONFDIR=$(dirname ${SCRIPT})
+SCRIPT=$(readlink -f "$0")
+CONFDIR=$(dirname "${SCRIPT}")
 
-for config in ${CONFDIR}/.*; do
-	BASE=$(basename ${config})
+for config in "${CONFDIR}"/.* "${CONFDIR}"/.config/*; do
+	BASE=${config#"${CONFDIR}/"}
 	HOMEBASE=${HOME}/${BASE}
 	BACKUP="${HOMEBASE}.$(date --rfc-3339=seconds)"
 
-	if [ "${BASE}" == ".git" ] || [ "${BASE}" == "." ] || [ "${BASE}" == ".." ]; then
+	if [ "${BASE}" == ".git" ] || [ "${BASE}" == ".config" ] || [ "${BASE}" == "." ] || [ "${BASE}" == ".." ]; then
 		continue
 	fi
 
-	if [ -e ${HOMEBASE} ] && [ ! -L ${HOMEBASE} ]; then
-		mv ${HOMEBASE} "${BACKUP}"
+	if [ -e "${HOMEBASE}" ] && [ ! -L "${HOMEBASE}" ]; then
+		mv "${HOMEBASE}" "${BACKUP}"
 		echo "Backup of ${HOMEBASE} created -> ${BACKUP}"
 	fi
 
-	if [ -L ${HOMEBASE} ]; then
-		rm ${HOMEBASE}
-		echo "Recreated: ${HOMEBASE}"
+	if [ -L "${HOMEBASE}" ]; then
+		if [[ "$1" == "--force" ]]; then
+			echo "Recreated: ${config} -> ${HOMEBASE}"
+			ln -sf "${config}" "${HOMEBASE}"
+		else
+			echo "Skipped: ${config} -> ${HOMEBASE}"
+		fi
 	else
-		echo "Created (new): ${HOMEBASE}"
+		echo "Created (new): ${config} -> ${HOMEBASE}"
+		ln -s "${config}" "${HOMEBASE}"
 	fi
-	ln -sf ${config} ${HOMEBASE}
 	if [ "${BASE}" == ".ssh" ] && [ -d "${BACKUP}" ]; then
 		echo "Copy content of ${BACKUP} back to ${HOMEBASE}"
 		cp -a "${BACKUP}/." "${HOMEBASE}/."
@@ -32,3 +36,4 @@ done
 
 exit 0
 
+# vim: set noexpandtab:
